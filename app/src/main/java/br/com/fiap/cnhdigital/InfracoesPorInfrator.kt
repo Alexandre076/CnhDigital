76f3d5af3e7f52fe2804bd9422c96a6bf077a3ab
Alexandre <all.martins076@gmail.com>
2022-08-24 21:31:57 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/cnhdigital/InfracoesPorInfrator.kt
@@ -0,0 +1,11 @@
+package br.com.fiap.cnhdigital
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InfracoesPorInfrator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_infracoes_por_infrator)
+    }
+}
